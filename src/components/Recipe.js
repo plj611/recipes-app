a07238b35dfe70bc95eafc18ddb11e456ff4c004
PLaw <plj611@outlook.com>
2024-04-09 14:28:13 +0800

    add star rating

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,14 +1,14 @@
 import React from "react";
 import IngredientsList from "./IngredientsList";
 import Instructions from "./Instructions";
-
+import StarRating from "./StarRating";
 
 function Recipe({ name, ingredients, steps }) {
 return (
 <section id={name.toLowerCase().replace(/ /g, "-")}>
 <Instructions title={name} steps={steps} />
 <IngredientsList list={ingredients} />
-
+<StarRating totalStars={5} />
 </section>
 );
 };
