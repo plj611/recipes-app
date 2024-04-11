7808edca6949f20f0490375b31e8b4e7e95e924c
PLaw <plj611@outlook.com>
2024-04-11 18:24:13 +0800

    start adding color rating

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,16 +1,14 @@
 import React from "react";
 import Recipe from "./Recipe";
-function Menu({ recipes }) {
+function Menu({recipes}) {
 return (
 <article>
 <header>
 <h1>Delicious Recipes</h1>
 </header>
-<div className="recipes">
-{recipes.map((recipe, i) => (
-<Recipe key={i} {...recipe} />
-))}
-</div>
+    <div className="recipes">
+        {recipes.data.map((recipe, i) => (<Recipe key={i} {...recipe} />))}
+    </div>
 </article>
 );
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 import Menu from "./components/Menu";
 import data from "./data/recipes.json";
+import colorData from "./data/color-data.json";
 
-render(<Menu recipes={data} />, document.getElementById("root"));
+render(<Menu recipes={{data, colorData}} />, document.getElementById("root"));
