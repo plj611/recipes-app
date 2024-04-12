a68db6d22ca1f87d367b093be84b23cf54ce755a
PLaw <plj611@outlook.com>
2024-04-12 18:18:46 +0800

    add trash can to remove color

--- src/components/Color.js
+++ src/components/Color.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { FaTrash } from "react-icons/fa";
 import StarRating2 from "./StarRating2";
 
 export default function Color({ title, color, rating }) {
     return (
     <section>
         <h1>{title}</h1>
+        <button>
+            <FaTrash />
+        </button>
         <div style={{ height: 50, backgroundColor: color }} />
         <StarRating2 selectedStars={rating} />
     </section>
