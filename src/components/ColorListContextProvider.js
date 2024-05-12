2225b67c76ed34148e81e0625273d4f5401c7faa
PLaw <plj611@outlook.com>
2024-05-12 22:59:52 +0800

    add ColorListContextProvider

--- src/components/ColorListContextProvider.js
+++ src/components/ColorListContextProvider.js
@@ -1,1 +1,23 @@
-import React, {createContext} from "react";
+import React, {useState, createContext} from "react";
+
+const ColorListContext = createContext()
+
+// export data for Menu to use
+// import the data in this module
+
+export function ColorListContextProvider({recipes, child}) {
+    const [colors, setColors] = useState(recipes.colorData);
+    const onRemoveColor = (id) => {const newColor = colors.filter(color => color.id !== id);
+                                                    setColors(newColor)};
+    const onRateColorList = (i, id) => {console.log(`Inside Menu ID: ${id} I: ${i}`);
+                                        const newColor = colors.map(color => {  if (color.id === id) {  color.rating = i;
+                                                                                                        return color;
+                                                                                                     }
+                                                                                return color;
+                                                                             })
+                                        setColors(newColor)                                    
+                                        }
+    return (<ColorListContext.Provider value={{colors, onRemoveColor, onRateColorList}}> 
+            {child}
+            </ColorListContext.Provider>)
+}
