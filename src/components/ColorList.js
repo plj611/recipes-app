9b6717e8ac8580226275da8cde638e614dc6503d
PLaw <plj611@outlook.com>
2024-05-15 23:09:37 +0800

    debug ColorListContextProvider

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,30 +1,28 @@
 import React, {useContext} from "react";
-// import Color from "./Color";
+import Color from "./Color";
 // import { colorListContext } from "./Menu";
 import { ColorListContextHook } from "./ColorListContextProvider";
 
 // export default function ColorList({ onRemoveColor = f=>f, onRateColorList = f=>f }) {
 export default function ColorList() {
-    /*
+    
     //const {colors} = useContext(colorListContext);
-    const {colors, abc} = ColorListContextHook();
-     const a = 1 / 0;
-
+    const {colors} = ColorListContextHook();
     if(!colors.length) 
         return <div>No Colors Listed.</div>;
     return (
-        <div>Hello
-        {
+        <div>Hello, 
+        { colors.length
             /*
             colors.map(color => <Color key={color.id} {...color} />)
             /*
             colors.map(color => <Color key={color.id} {...color} onRemove={onRemoveColor} 
                                         onRateColor={(i, id) => {onRateColorList(i, id); 
                                                                        console.log(`Inside ColorList.js I: ${i} ID: ${id}`)}}/>)
-                                        
+            */                            
         }
         </div>
     );
-    */
-   return (<div>Hello World again!</div>)
+    
+   // return (<div>Hello World again!</div>)
 }
